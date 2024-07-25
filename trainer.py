573e06937697a7b0ac0fbd30e9ac72711977b2a0
Mine268 <mine268@foxmail.com>
2024-07-25 10:28:23 +0800

    init commit

---
+++ trainer.py
@@ -0,0 +1,8 @@
+from typing import *
+
+import einops as eps
+import torch
+import torch.nn as nn
+import numpy as np
+import pytorch_lightning as pl
+from timm.models.vision_transformer import Block
