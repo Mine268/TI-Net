447b1d03769bf496e082dc9d9b811eeddecb45aa
Mine268 <mine268@foxmail.com>
2024-08-12 14:26:40 +0800

    vanilla vit ae

--- trainer.py
+++
@@ -1,8 +0,0 @@
-from typing import *
-
-import einops as eps
-import torch
-import torch.nn as nn
-import numpy as np
-import pytorch_lightning as pl
-from timm.models.vision_transformer import Block
