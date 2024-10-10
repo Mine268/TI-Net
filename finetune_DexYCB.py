fix bugs of finetuning: original code would compute the gradient for backbone and update it, thus it would cause the problem. Fixing this bug accelerate the training.

finetune_DexYCB.py
@@ -125,8 +125,8 @@ def train_one_epoch(model: torch.nn.Module,
     for data_iter_step, data_item in enumerate(
         metric_logger.log_every(data_loader, print_freq, header)):
         # we use a per iteration (instead of per epoch) lr scheduler
-        if data_iter_step % accum_iter == 0:
-            lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
+        # if data_iter_step % accum_iter == 0:
+        #     lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
 
         images = data_item['image'].to(device)
         pose_gt = data_item['pose'].to(device)
@@ -143,6 +143,17 @@ def train_one_epoch(model: torch.nn.Module,
 
         if not math.isfinite(loss_value):
             print("Loss is {}, stopping training".format(loss_value))
+            # dump the error checkpoint
+            to_save = {
+                "model": model.state_dict(),
+                "optimizer": optimizer.state_dict(),
+                "epoch": epoch,
+                "scaler": loss_scaler.state_dict(),
+                "args": args,
+                "input": data_item,
+                "loss": loss
+            }
+            torch.save(to_save, os.path.join(args.output_dir, "error_checkpoint.pth"))
             sys.exit(1)
 
         for k in loss.keys():
@@ -258,6 +269,7 @@ def main(args):
     elif model_class == 'resnet':
         model = resnet.__dict__[model_arch](predict_mano=True,
                                             backbone_ckpt=args.backbone_ckpt)
+        print(f"Use pretrained backbone: {args.backbone_ckpt}")
     else:
         assert False, "model not supported: %s" % model_str
 

resnet/posenet.py
@@ -63,20 +63,6 @@ class PoseResNet(nn.Module):
         else:
             self.backbone.train()
 
-    def train(self, mode: bool = True):
-        if not self.pretrained_backbone:
-            self.backbone.train(mode)
-        self.pose_mlp.train(mode)
-        print(f"trigger TRAIN mode, train backbone: {(not self.pretrained_backbone) and mode}")
-        return self
-
-    def eval(self):
-        if not self.pretrained_backbone:
-            self.backbone.eval()
-        self.pose_mlp.eval()
-        print(f"trigger EVAL mode")
-        return self
-
     def extract_feature(self, x):
         x = self.backbone.conv1(x)
         x = self.backbone.bn1(x)
@@ -93,7 +79,11 @@ class PoseResNet(nn.Module):
         return einops.rearrange(self.pose_mlp(feats), "b (j d) -> b j d", d=3)
 
     def forward(self, imgs):
-        feats = self.extract_feature(imgs)
+        if self.pretrained_backbone:
+            with torch.no_grad():
+                feats = self.extract_feature(imgs)
+        else:
+            feats = self.extract_feature(imgs)
         pose = self.decode_pose(feats)
         return pose
 