2955cf405cdaca01e4fc63e9b6344e9c2d9142a0
Mine268 <renkaiwen23@mails.ucas.ac.cn>
2024-11-02 17:18:24 +0800

    ti_vit

--- vit/ti_vit.py
+++ vit/ti_vit.py
@@ -9,14 +9,15 @@
 # DeiT: https://github.com/facebookresearch/deit
 # --------------------------------------------------------
 
-from functools import partial
-
+import einops
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from timm.models.vision_transformer import PatchEmbed, Block
 
 from .pos_embed import get_2d_sincos_pos_embed
+import utils
 
 
 class TI_ViT(nn.Module):
@@ -25,8 +26,24 @@ class TI_ViT(nn.Module):
     def __init__(self, img_size=224, patch_size=16, in_chans=3,
                  embed_dim=1024, depth=24, num_heads=16,
                  decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
-                 mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False):
+                 mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False,
+                 train_secondary_trans=True):
         super().__init__()
+        self.t_num = 14 if train_secondary_trans else 4
+        self.train_secondary_trans = train_secondary_trans
+
+        # --------------------------------------------------------------------------
+        # Isomorphic Transformations
+        self.rot_embed = nn.Sequential(nn.Linear(2, 32, bias=True), nn.ReLU(),
+                                       nn.Linear(32, 32, bias=True), nn.ReLU(),
+                                       nn.Linear(32, 32, bias=False))
+        self.horizontal_flip = nn.Sequential(nn.Linear(embed_dim, 128, bias=False),
+                                             nn.Linear(128, embed_dim, bias=False))
+        self.rotation = nn.Sequential(nn.Linear(embed_dim + 32, 128, bias=False),
+                                      nn.Linear(128, embed_dim, bias=False))
+        self.horizontal_rot = nn.Sequential(nn.Linear(embed_dim + 32, 128, bias=False),
+                                            nn.Linear(128, embed_dim, bias=False))
+        # --------------------------------------------------------------------------
 
         # --------------------------------------------------------------------------
         # MAE encoder specifics
@@ -91,7 +108,36 @@ class TI_ViT(nn.Module):
                 nn.init.constant_(m.bias, 0)
         elif isinstance(m, nn.LayerNorm):
             nn.init.constant_(m.bias, 0)
-            nn.init.constant_(m.weight, 1.0)
+            nn.init.constant_(m.weight, 1.0)    
+
+    def latent_operate(self, operate: str, latent: torch.Tensor, rot: torch.Tensor = None):
+        '''
+        operate:
+         - h: horizontal flip, rot=None
+         - r: rotation, rot required
+         - hr: hflip & rot, rot required
+        
+        latent: [B D]
+        
+        rot: [1] / [B]
+        '''
+        assert operate in ['h', 'r', 'hr'], f"operate '{operate}' is not supported."
+        assert not (operate in ['r', 'hr'] and rot is None), f"rot required."
+
+        if operate == 'h':
+            return latent + self.horizontal_flip(latent)
+        
+        # encode rotation
+        rot = rot if rot.shape[0] == latent.shape[0] else rot.repeat(latent.shape[0])
+        rot.to(latent.device)
+        rot_vec = torch.cat([torch.cos(rot)[...,None], torch.sin(rot)[...,None]], dim=-1)
+        rot_embedding = self.rot_embed(rot_vec)
+        
+        if operate == 'r':
+            return latent + self.rotation(torch.cat([latent, rot_embedding], dim=-1))
+        
+        if operate == 'hr':
+            return latent + self.horizontal_rot(torch.cat([latent, rot_embedding], dim=-1))
 
     def patchify(self, imgs):
         """
@@ -172,6 +218,7 @@ class TI_ViT(nn.Module):
 
     def forward_decoder(self, x, cls_token, ids_restore):
         # embed tokens
+        cls_token = self.decoder_embed(cls_token)
         x = self.decoder_embed(x)
 
         # append mask tokens to sequence
@@ -196,27 +243,121 @@ class TI_ViT(nn.Module):
 
         return x
 
-    def forward_loss(self, imgs, pred, mask):
+    def forward_loss(self, imgs, pred, mask, latent, a1, a2, b1, b2):
         """
         imgs: [N, 3, H, W]
         pred: [N, L, p*p*3]
         mask: [N, L], 0 is keep, 1 is remove, 
         """
+        # ----------------------------------------------------------------------
+        # 1. Reconstruction loss
         target = self.patchify(imgs)
         if self.norm_pix_loss:
             mean = target.mean(dim=-1, keepdim=True)
             var = target.var(dim=-1, keepdim=True)
             target = (target - mean) / (var + 1.e-6)**.5
 
-        loss = (pred - target) ** 2
-        loss = loss.mean(dim=-1)  # [N, L], mean loss per patch
+        recon_loss = (pred - target) ** 2
+        recon_loss = recon_loss.mean(dim=-1)  # [N, L], mean loss per patch
 
-        loss = (loss * mask).sum() / mask.sum()  # mean loss on removed patches
-        return loss
+        recon_loss = (recon_loss * mask).sum() / mask.sum()  # mean loss on removed patches
+        # ----------------------------------------------------------------------
+        
+        # ----------------------------------------------------------------------
+        # 2. Isomorphism loss
+        # (0) Identity operation
+        loss_iden = F.mse_loss(self.latent_operate('r', latent[0], torch.zeros_like(a1)), latent[0])
+        # (1) Uniary operation
+        loss_uni_h = F.mse_loss(self.latent_operate('h', latent[0]), latent[1])
+        if self.train_secondary_trans:
+            loss_uni_r = F.mse_loss(self.latent_operate('r', latent[0], a1), latent[2]) + F.mse_loss(self.latent_operate('r', latent[0], b1), latent[4])
+            loss_uni_hr = F.mse_loss(self.latent_operate('hr', latent[0], a2), latent[3]) + F.mse_loss(self.latent_operate('hr', latent[0], b2), latent[5])
+        else:
+            loss_uni_r = F.mse_loss(self.latent_operate('r', latent[0], a1), latent[2])
+            loss_uni_hr = F.mse_loss(self.latent_operate('hr', latent[0], a2), latent[3])
+        loss_uni = loss_uni_h + loss_uni_r + loss_uni_hr
+        # (2) Binary operation
+        if self.train_secondary_trans:
+            loss_bin_h_h = F.mse_loss(self.latent_operate('h', self.latent_operate('h', latent[0])), latent[0])
+            loss_bin_h_r = F.mse_loss(self.latent_operate('r', self.latent_operate('h', latent[0]), b1), latent[6])
+            loss_bin_h_hr = F.mse_loss(self.latent_operate('hr', self.latent_operate('h', latent[0]), b2), latent[8])
+            loss_bin_r_h = F.mse_loss(self.latent_operate('h', self.latent_operate('r', latent[0], a1)), latent[7])
+            loss_bin_r_r = F.mse_loss(self.latent_operate('r', self.latent_operate('r', latent[0], a1), b1), latent[9])
+            loss_bin_r_hr = F.mse_loss(self.latent_operate('hr', self.latent_operate('r', latent[0], a1), b2), latent[11])
+            loss_bin_hr_h = F.mse_loss(self.latent_operate('h', self.latent_operate('hr', latent[0], a2)), latent[10])
+            loss_bin_hr_r = F.mse_loss(self.latent_operate('r', self.latent_operate('hr', latent[0], a2), b1), latent[12])
+            loss_bin_hr_hr = F.mse_loss(self.latent_operate('hr', self.latent_operate('hr', latent[0], a2), b2), latent[13])
+            loss_bin = loss_bin_h_h + loss_bin_h_r + loss_bin_h_hr + loss_bin_r_h + loss_bin_r_r + \
+                       loss_bin_r_hr + loss_bin_hr_h + loss_bin_hr_r + loss_bin_hr_hr
+        # (3) Total loss
+        if self.train_secondary_trans:
+            latent_loss = loss_iden + loss_uni + loss_bin
+        else:
+            latent_loss = loss_iden + loss_uni
+        # ----------------------------------------------------------------------
+
+        loss = recon_loss + 1e-3 * latent_loss
+        return {
+            "backward": loss,
+            "reconstruction": recon_loss,
+            "latent": latent_loss
+        }
 
     def forward(self, imgs, mask_ratio=0.75):
-        latent, mask, ids_restore = self.forward_encoder(imgs, mask_ratio)
-        cls_token = torch.mean(latent, dim=1, keepdim=True)
-        pred = self.forward_decoder(latent, cls_token, ids_restore)  # [N, L, p*p*3]
-        loss = self.forward_loss(imgs, pred, mask)
-        return loss, pred, mask
+        '''
+        imgs [B,3,H,W]
+        '''
+        a1 = torch.rand(size=(imgs.shape[0],), device=imgs.device) * 2 * torch.pi
+        a2 = torch.rand(size=(imgs.shape[0],), device=imgs.device) * 2 * torch.pi
+        b1 = torch.rand(size=(imgs.shape[0],), device=imgs.device) * 2 * torch.pi
+        b2 = torch.rand(size=(imgs.shape[0],), device=imgs.device) * 2 * torch.pi
+
+        # ordinary transformation
+        imgs_h = utils.horizontal_flip_img(imgs).detach()
+        imgs_r_a1 = utils.rotate_img(imgs, a1).detach()
+        imgs_hr_a2 = utils.hflip_rotate_img(imgs, a2).detach()
+        imgs_r_b1 = utils.rotate_img(imgs, b1).detach()
+        imgs_hr_b2 = utils.hflip_rotate_img(imgs, b2).detach()
+
+        # secondary transformation
+        imgs_hr_b1 = utils.hflip_rotate_img(imgs, b1).detach()
+        imgs_hr_na1 = utils.hflip_rotate_img(imgs, -a1).detach()
+        imgs_r_b2 = utils.rotate_img(imgs, b2).detach()
+        imgs_r_a1_b1 = utils.rotate_img(imgs, a1 + b1).detach()
+        imgs_r_na2 = utils.rotate_img(imgs, -a2).detach()
+        imgs_hr_na1_b2 = utils.hflip_rotate_img(imgs, -a1 + b2).detach()
+        imgs_hr_a2_b1 = utils.hflip_rotate_img(imgs, a2 + b1).detach()
+        imgs_r_na2_b2 = utils.rotate_img(imgs, -a2 + b2).detach()
+
+        # batch-up images
+        imgs_batch = einops.rearrange(
+            torch.cat([imgs[:,None],  # 0
+                       imgs_h[:,None],
+                       imgs_r_a1[:,None],  # 2
+                       imgs_hr_a2[:,None],
+                       imgs_r_b1[:,None],  # 4
+                       imgs_hr_b2[:,None],
+                       imgs_hr_b1[:,None],  # 6
+                       imgs_hr_na1[:,None],
+                       imgs_r_b2[:,None],  # 8
+                       imgs_r_a1_b1[:,None],
+                       imgs_r_na2[:,None],  # 10
+                       imgs_hr_na1_b2[:,None],
+                       imgs_hr_a2_b1[:,None],  # 12
+                       imgs_r_na2_b2[:,None]],
+                       dim=1)[:,:self.t_num],
+            'b t ... -> (b t) ...', t=self.t_num
+        )
+
+        latents, masks, ids_restores = self.forward_encoder(imgs_batch, mask_ratio)
+        cls_tokens = torch.mean(latents, dim=1, keepdim=False)
+        preds = self.forward_decoder(latents, cls_tokens[:,None], ids_restores)
+        losses = self.forward_loss(imgs_batch, preds, masks,
+                                   einops.rearrange(cls_tokens, '(b t) ... -> t b ...', t=self.t_num),
+                                   a1, a2, b1, b2)
+
+        pred_0 = einops.rearrange(preds, '(b t) ... -> b t ...', t=self.t_num)
+        pred_0 = pred_0[:, 0].detach()
+        pred_0 = self.unpatchify(pred_0)
+
+        return losses, pred_0
